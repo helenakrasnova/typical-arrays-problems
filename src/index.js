238773605d0902eea11c1abac398e6ea462b8a1a
Helena Krasnova <alenka5354@mail.ru>
2020-09-29 01:10:01 +0300

    feat: implemented  min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
 }
